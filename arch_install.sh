51ba912d58fbdc129668b97c978d7a12147844c2
Metalszorny <renden@freemail.hu>
2026-03-19 17:25:04 +0000

    First steps

--- arch_install.sh
+++ arch_install.sh
@@ -1,2 +1,30 @@
 #!/usr/bin/env bash
 #!/usr/bin/env
+
+WIRELESS_DEVICE_NAME="wlan0"
+WIRELESS_SSID=""
+WIRELESS_PASSWORD=""
+
+##########
+# Set installation keymap
+##########
+#localectl list-keymaps
+#localectl list-keymaps | grep -i hu
+loadkeys uk
+
+##########
+# Update system clock
+##########
+timedatectl set-ntp true
+
+##########
+# Set up network connection
+##########
+iwctl
+#device list
+#device [selected_station] set-property Powered on
+#station [WIRELESS_DEVICE_NAME] scan
+#station [WIRELESS_DEVICE_NAME] get-networks
+station [WIRELESS_DEVICE_NAME] connect [WIRELESS_SSID] --passphrase [WIRELESS_PASSWORD]
+exit
+#ip addr show
